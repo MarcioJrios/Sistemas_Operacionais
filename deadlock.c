dae1c65080c97838334d89b16173aae84263dd22
Marcio J. Rios <31110984+MarcioJrios@users.noreply.github.com>
2020-10-07 15:46:52 -0300

    Tentativa 2

--- deadlock.c
+++ deadlock.c
@@ -51,22 +51,21 @@ void *thread1(void *data){
 
 void *thread2(void *data){
      unsigned long i,j;
-     while(1){
+     //while(1){
      if(pthread_mutex_lock(&mutex2)==0){
          printf("Thread ID%ld got mutex2.\n", pthread_self());
          for(i=0; i< 10000000; ++i); // just for wasting some time
-         if(pthread_mutex_trylock(&mutex1)==0){
+         if(pthread_mutex_lock(&mutex1)==0){
             printf("Thread ID%ld got mutex1.\n", pthread_self());          
             for(i=0; i< 10000000; ++i); // just for wasting some time
-            pthread_mutex_unlock(&mutex1); 
-            break;  
+            pthread_mutex_unlock(&mutex1);  
          }
          else { printf("\nThread ID%ld did not get mutex1.\n", pthread_self()); pthread_mutex_unlock(&mutex1);
                 pthread_mutex_unlock(&mutex2);}
          //pthread_mutex_unlock(&mutex2);
      }
      else { printf("\nThread ID%ld did not get mutex2.\n", pthread_self()); pthread_mutex_unlock(&mutex2);}
-     }
+     //}
      pthread_mutex_unlock(&mutex2);
      pthread_exit(NULL);
 }
