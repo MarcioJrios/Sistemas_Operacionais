be09c1b8137eee1b2e19422cf8e2878aa49766cb
Marcio J. Rios <31110984+MarcioJrios@users.noreply.github.com>
2020-10-22 15:56:00 -0300

    Jantar dos Filósofos Multithread

---
+++ filosofos.c
@@ -0,0 +1,106 @@
+/* Dica para compilação: 
+                         gcc -o test2 MutexESemaforo.c -lpthread */
+
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <semaphore.h>      
+#include <fcntl.h> 
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+#define N 5 // number of threads
+#define THINKING 0
+#define HUNGRY 1
+#define EATING 2
+#define LEFT (i + N - 1) % N //pegar garfo da esquerda
+#define RIGHT (i + 1) % N    //pegar garfo da direita
+
+void *mythread(void *data);
+void take_forks(int);
+void put_forks(int);
+void test(int);
+
+int nfilosofo[N] = {0, 1, 2, 3, 4};
+int state[N] = {0, 0, 0, 0, 0}; // estados dos philosophers (comendo, pensando e com fome)
+sem_t phil_sem[N];              // controlador de cada filosofo
+
+sem_t s;
+
+
+int x = 0; 
+
+
+
+void think(int i){
+    printf("Filosofo %d esta pensando.\n", i);
+    int t = (rand()%5);
+    sleep(t);
+}
+
+void eat(int i){
+    printf("Filosofo %d esta comendo.\n", i);
+    int t = (rand()%5);
+    sleep(t);
+}
+
+void *mythread(void *data)
+{
+    while (1)
+    {
+        int *id = data;
+        think(*id);
+        take_forks(*id);
+        eat(*id);
+        put_forks(*id);
+    }
+}
+
+void take_forks(int i)
+{
+    pthread_mutex_lock(&mutex);
+    state[i] = HUNGRY;
+    test(i);
+    pthread_mutex_unlock(&mutex);
+    sem_wait(&phil_sem[i]);
+}
+
+void put_forks(int i)
+{
+    pthread_mutex_lock(&mutex);
+    state[i] = THINKING;
+    printf("Filosofo %d deixou os garfos %d e %d.\n", i, LEFT, RIGHT);
+    test(LEFT);
+    test(RIGHT);
+    pthread_mutex_unlock(&mutex);
+}
+
+void test(int i)
+{
+    if (state[i] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING)
+    {
+        state[2] = HUNGRY;
+        printf("Filosofo %d pegou os garfos %d e %d.\n", i, LEFT, RIGHT);
+        sem_post(&phil_sem[i]);
+    }
+}
+
+int main(void)
+{
+    int i = 0;
+    pthread_t filosofo[N];
+    for (i = 0; i < N; i++)
+        sem_init(&phil_sem[i], 0, 0); // inicializa o semáforo
+    for (i = 0; i < N; i++)
+    {
+        int *j = malloc(sizeof(int));
+        *j = i;
+        pthread_create(&filosofo[i], NULL, mythread, &nfilosofo[i]);
+    }
+    for (i = 0; i < N; i++)
+        pthread_join(filosofo[i], NULL);
+    return (1);
+}
